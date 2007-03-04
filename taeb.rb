e255d39cef22f4cc6565e9325b05dc2a538840e4
sartak <sartak@gmail.com>
2007-03-03 21:20:46 -0500

    Enter the game and quit; show what we're sending at each keystroke

--- taeb.rb
+++ taeb.rb
@@ -3,6 +3,12 @@ require 'interface/telnet.rb'
 password = File.new("password.txt").readline
 
 connection = Telnet.new()
-output = connection.send_and_recv("lTAEB\n" + password + "\n")
-print output
+commands = ["l", "TAEB\n", password + "\n", "pnmmn", "    ", "#quit\n", "y", "          "]
+
+for command in commands
+  for c in command.split(//)
+    puts "send('" + c + "')" unless command == commands[2]
+    connection.send_and_recv(c)
+  end
+end
 
