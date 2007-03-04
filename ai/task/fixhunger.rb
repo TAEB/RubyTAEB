e0da93f426f1322e7f6d4c5a22d146039f3af960
sartak <sartak@gmail.com>
2007-03-04 04:02:38 -0500

    Pray only if weak, add fainting? and weak? methods

--- ai/task/fixhunger.rb
+++ ai/task/fixhunger.rb
@@ -4,13 +4,21 @@ class TaskFixHunger
   def initialize()
   end
 
+  def fainting?()
+    $controller.vt.to_s =~ /Fainting|Fainted/
+  end
+
+  def weak?()
+    $controller.vt.to_s =~ /Weak|Fainting|Fainted/
+  end
+
   def hungry?()
-    $controller.vt.to_s =~ /Hungry|Weak|Fainting/
+    $controller.vt.to_s =~ /Hungry|Weak|Fainting|Fainted/
   end
 
   def run()
-    if hungry?
-      $stderr.puts "Praying because we're hungry"
+    if weak?
+      $stderr.puts "Praying because we're weak"
       $controller.send("#pray\n")
       return 1
     end
