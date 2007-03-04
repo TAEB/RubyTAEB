8591ff8ab73032f36d9ff5a3ea75d77b08a6393f
sartak <sartak@gmail.com>
2007-03-04 03:34:28 -0500

    Add TaskFight and TaskOpenDoor

--- taeb.rb
+++ taeb.rb
@@ -3,12 +3,16 @@ require 'interface/controller.rb'
 require 'interface/telnet.rb'
 require 'interface/vt.rb'
 require 'ai/task/explore.rb'
+require 'ai/task/fight.rb'
+require 'ai/task/opendoor.rb'
 require 'ai/hero.rb'
 
 begin
   $controller = Controller.new()
   $hero = Hero.new()
   $task_explore = TaskExplore.new()
+  $task_fight = TaskFight.new()
+  $task_opendoor = TaskOpenDoor.new()
 
   while 1
     if $controller.vt.to_s =~ /--More--/
@@ -17,6 +21,8 @@ begin
       redo
     end
 
+    redo if $task_fight.run()
+    redo if $task_opendoor.run()
     redo if $task_explore.run()
   end
 
