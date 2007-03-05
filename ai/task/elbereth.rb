c9e7c368b073d7c88c1d958ceffec2a5b0d002a8
sartak <sartak@gmail.com>
2007-03-05 01:35:10 -0500

    Create TaskElbereth

---
+++ ai/task/elbereth.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+require 'ai/task/base.rb'
+
+class TaskElbereth < BaseTask
+  def priority()
+    $controller.vt.row(23) =~ /HP:(\d+)\((\d+)\)/ or raise "Unable to check HP"
+    $1.to_i * 2 <= $2.to_i ? 1 : 0
+  end
+
+  def pick_move()
+    "E-  Elbereth\n"
+  end
+end
+
