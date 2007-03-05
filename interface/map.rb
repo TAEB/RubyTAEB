d7f50224e71dd1c17c52807f6c369222b908e5d7
sartak <sartak@gmail.com>
2007-03-04 19:18:25 -0500

    Begin refactoring of Map code

--- interface/map.rb
+++ interface/map.rb
@@ -1,26 +1,50 @@
 #!/usr/bin/ruby
 
 class Map
+  attr_reader :updated_this_turn
+
   def initialize()
-    @map = Array.new(81) {|z| Array.new(24) {|x| Array.new(55, "\0")}}
+    @map = []
     @z = 1
+    initialize_level(@z)
+    @updated_this_turn = 0
   end
 
-  def descend
-    @z += 1
+  def initialize_level(z)
+    @map[z] = Array.new(24) {|x| Array.new(55) {|y| Tile.new() }}
   end
 
-  def scenery?(tile)
-    case tile
-    when '.', ',', ']', '-', '|', '#', '}', '{', '<', '>',
-      1
-    else
-      nil
+  def each_adjacent()
+    for dx in [-1, 0, 1]
+      for dy in [-1, 0, 1]
+        yield dx, dy unless dx == 0 and dy == 0
+      end
+    end
+  end
+
+  def travel(x0, y0, x1, y1)
+    queue = [[x0, y0, []]]
+    visited = Array.new(81) {|z| Array.new(24) {|x| Array.new(55, false)}}
+    visited[x0][y0] = true
+
+    while queue.size > 0
+      x, y, path = shift
+      if x == x1 and y == y1
+        return path
+      end
+      each_adjacent do |dx, dy|
+      end
     end
   end
 
   def update(x=nil, y=nil, tile=nil)
+    @updated_this_turn = 0
+    $controller.vt.to_s =~ /Dlvl:(\d+)/ or raise "Unable to check dungeon level"
+    @z = $1.to_i
     if (x != nil && y != nil && tile != nil)
+      if @map[x][y][@z] != tile
+        @updated_this_turn = 1
+      end
       @map[x][y][@z] = tile
       return
     end
@@ -28,6 +52,9 @@ class Map
     0.upto(80) do |x|
       0.upto(24) do |y|
         if scenery?($controller.vt.at(x, y))
+          if @map[x][y][@z] != $controller.vt.at(x, y)
+            @updated_this_turn = 1
+          end
           @map[x][y][@z] = $controller.vt.at(x, y)
         end
       end
