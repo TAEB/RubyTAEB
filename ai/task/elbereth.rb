1f6d3773fba7e1aeae32c743b7c95e357cce74dc
sartak <sartak@gmail.com>
2007-03-05 01:40:02 -0500

    Make it so HP3(100) is matched as well as HP:3(100) (shrug)

--- ai/task/elbereth.rb
+++ ai/task/elbereth.rb
@@ -3,7 +3,7 @@ require 'ai/task/base.rb'
 
 class TaskElbereth < BaseTask
   def priority()
-    $controller.vt.row(23) =~ /HP:(\d+)\((\d+)\)/ or raise "Unable to check HP"
+    $controller.vt.row(23) =~ /HP:?(\d+)\((\d+)\)/ or raise "Unable to check HP"
     $1.to_i * 2 <= $2.to_i ? 1 : 0
   end
 
