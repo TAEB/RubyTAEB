a0dfa257d338d5fc07a8486e0eee4acdf0594416
sartak <sartak@gmail.com>
2007-03-04 23:23:25 -0500

    Add a TaskFight

---
+++ ai/task/fight.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+require 'ai/task/base.rb'
+
+class TaskFight < BaseTask
+  def initialize()
+    @direction = nil
+  end
+
+  def adjacent_fightable?()
+    $map.each_direction do |dx, dy|
+      if $map.at_delta(dx, dy).monster?
+        @direction = $map.move_with_delta(dx, dy)
+        return true
+      end
+    end
+    false
+  end
+
+  def priority()
+    adjacent_fightable?() ? 1 : 0
+  end
+
+  def pick_move()
+    "F" + @direction
+  end
+end
+
+
+
