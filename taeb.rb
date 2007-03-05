51647dc29faaec4a58e5b1ee819c655a9975fe06
sartak <sartak@gmail.com>
2007-03-04 21:21:07 -0500

    Refactoring of taeb.rb, includes "Things that are here" handling, uses TaskHandler

--- taeb.rb
+++ taeb.rb
@@ -1,24 +1,35 @@
 #!/usr/bin/ruby
-require 'interface/controller.rb'
-require 'interface/telnet.rb'
-require 'interface/vt.rb'
-#require 'interface/map.rb'
-require 'interface/menu.rb'
-require 'ai/hero.rb'
+
+$:.push('interface', 'ai', 'ai/task')
+require 'debug.rb'
+require 'controller.rb'
+require 'map.rb'
+require 'menu.rb'
+require 'hero.rb'
+require 'taskhandler.rb'
 
 begin
   $controller = Controller.new()
   $hero = Hero.new()
-  #$map = Map.new()
+  $map = Map.new()
+  $taskhandler = TaskHandler.new()
 
   while 1
-    if $controller.vt.to_s =~ /--More--/
-      $stderr.puts "I see a --More--!"
+    if $controller.vt.row(0) =~ /^ *Things that are here: *$/
+      debug("Things that are here menu")
       $controller.send(" ")
       redo
     end
-    if $controller.vt.to_s =~ /^Do you want your possessions identified\?/
-      $stderr.puts "Oh no! We died!"
+
+    if $controller.vt.row(0) =~ /--More-- *$/ or
+       $controller.vt.row(1) =~ /--More-- *$/
+      debug("I see a --More--!")
+      $controller.send(" ")
+      redo
+    end
+
+    if $controller.vt.row(0) =~ /^Do you want your possessions identified\?/
+      debug("Oh no! We died!")
       $controller.send("y")
       while 1 # let the Disconnect exception break the loop
         $controller.send(" ")
@@ -26,21 +37,8 @@ begin
       end
     end
 
-    #$map.update
-
-    sleep 2
-    $controller.send("O")
-    menu = Menu.new()
-    menu.select_all_matches(/auto/)
-    menu.toggle_all_matches(/pickup/)
-    menu.unselect_all_matches(/pickup_types/)
-    menu.execute()
-
-    # handle pickup_burden menu
-    menu = Menu.new()
-    menu.single_select(/unencumbered/)
-
-    $controller.send("Sy  ")
+    $map.update
+    $taskhandler.next_task()
   end
 
 rescue RuntimeError => err
