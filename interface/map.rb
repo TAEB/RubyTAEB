8c9a6d8b46e5f7e53b05aa85f57dc335b958ce33
sartak <sartak@gmail.com>
2007-03-04 21:48:44 -0500

    Apparently == is used for testing equality, not =

--- interface/map.rb
+++ interface/map.rb
@@ -133,7 +133,7 @@ class Map
       onscreen = $controller.vt.at(x, y)
 
       # assume the tile is very walkable until otherwise noticed
-      if not Tile.scenery?(onscreen) and @map[x][y][@z].scenery = nil
+      if not Tile.scenery?(onscreen) and @map[x][y][@z].scenery == nil
         onscreen = ','
       end
 
