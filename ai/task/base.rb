f4ba56390923b91aab3852b5a8b093bf5bfe5994
sartak <sartak@gmail.com>
2007-03-04 21:20:12 -0500

    Give BaseTask.run good default behavior, add next_move which is to be overridden

--- ai/task/base.rb
+++ ai/task/base.rb
@@ -9,7 +9,17 @@ class BaseTask
   end
 
   def run()
-    raise "You need to override BaseTask.run"
+    move = pick_move
+    if move == nil
+      return false
+    end
+
+    $controller.send(move)
+    return true
+  end
+
+  def pick_move()
+    raise "You need to override BaseTask.pick_move"
   end
 end
 
