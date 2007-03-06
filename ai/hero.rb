b8157f4dd408b0500495a93952d04f9e41a7aa65
jluehrs2 <jluehrs2@uiuc.edu>
2007-03-06 18:56:21 -0500

    Add prayertimeout(min|max) to hero

--- ai/hero.rb
+++ ai/hero.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/ruby
 
 class Hero
+  attr_accessor :prayertimeoutmin, :prayertimeoutmax
+
   def initialize()
     @x = @y = 0
+    @prayertimeoutmin = 300
+    @prayertimeoutmax = 500
   end
 
   def x()
