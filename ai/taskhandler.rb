4c10269eb6299fe520f623933f28cfd1a882d606
sartak <sartak@gmail.com>
2007-03-06 15:39:26 -0500

    Move DYWYPI handling to the top -- also get "Do you want to see what you had when you died?" if you're carrying no unID items

--- ai/taskhandler.rb
+++ ai/taskhandler.rb
@@ -41,6 +41,16 @@ class TaskHandler
   end
 
   def clear_screen()
+    if $controller.vt.row(0) =~ /^Do you want your possessions identified\?/ or
+       $controller.vt.row(0) =~ /^Do you want to see what you had when you died\?/
+      debug("Oh no! We died!")
+      $controller.send("y")
+      while 1 # let the Disconnect exception break the loop
+        $controller.send(" ")
+        sleep 1
+      end
+    end
+
     if $controller.vt.row(0) =~ /^ *Things that are here: *$/ or
        $controller.vt.row(2) =~ /^ *Things that are here: *$/
       debug("Things that are here menu")
@@ -52,15 +62,6 @@ class TaskHandler
          debug("I see a --More--!")
          $controller.send(" ")
          clear_screen()
-    end
-
-    if $controller.vt.row(0) =~ /^Do you want your possessions identified\?/
-      debug("Oh no! We died!")
-      $controller.send("y")
-      while 1 # let the Disconnect exception break the loop
-        $controller.send(" ")
-        sleep 1
-      end
     end
   end
 end
